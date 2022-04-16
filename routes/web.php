7ab0619c5a5fd759751c3f631e1e6befbce68060
is42-kobzevng <kobzew07@gmail.com>
2022-04-16 17:07:25 +0500

    version 1.5.2

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,34 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('my-name', function () {
+    return ('Кобзев Никита Григорьевич');
+});
+Route::get('my-friend', function () {
+    return ('Кайзерова Анна Евгеньевна');
+});
+Route::get('get-friend/{name}', function ($name) {
+    $name='Kaizer';
+    return $name;
+});
+Route::get('my-city/{city}', function ($city) {
+    return $city;
+});
+Route::get('level/{lvl}', function ($lvl) {
+
+    if($lvl>=0 && $lvl<=25){
+        echo 'Новичок!';
+    }
+    if($lvl>=26 && $lvl<=50){
+        echo 'Специалист!';
+    }
+    if($lvl>=51 && $lvl<=75){
+        echo 'Босс!';
+    }
+    if($lvl>=76 && $lvl<=98){
+        echo 'Старик!';
+    }
+    if($lvl==99){
+        echo 'Король!';
+    }
+});
